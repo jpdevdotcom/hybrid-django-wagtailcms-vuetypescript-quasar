4473a28de1358cc5c6d81bb16915e89425e17e43
Jan Phillip Dacallos <jphillipdacallos@gmail.com>
2025-06-23 20:59:32 +0800

    chore: add backend

---
+++ hybrid_2/backend/backend/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-&#-xt2@fb9_r6x&3&0(zl&1q_mpf8%^*n1fyq!q%iode_&3koc"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
